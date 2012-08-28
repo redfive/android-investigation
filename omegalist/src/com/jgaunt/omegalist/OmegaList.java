d2e1e64b57dc0af3caa8706a1dd403691cedfb62
John (redfive) Gaunt <redfive@songbirdnest.com>
2012-08-28 11:55:05 -0700

    Backed a listview with non-String objects - created inner class to store data - modified layout; added a ListView - updated manifest to get the action bar

--- omegalist/src/com/jgaunt/omegalist/OmegaList.java
+++ omegalist/src/com/jgaunt/omegalist/OmegaList.java
@@ -2,14 +2,41 @@ package com.jgaunt.omegalist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class OmegaList extends Activity
 {
+    public class OmegaListItem {
+        public String omegaName = "";
+
+        public OmegaListItem( String aName ) {
+            omegaName = aName;
+        }
+
+        public String toString () {
+            return omegaName;
+        }
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        OmegaListItem [] items = {
+                             new OmegaListItem("one"),
+                             new OmegaListItem("two"),
+                             new OmegaListItem("three") };
+
+        ArrayAdapter<OmegaListItem> listAdapter = new ArrayAdapter<OmegaListItem> (this, android.R.layout.simple_list_item_1, items);
+
+        ListView lView = (ListView) findViewById(R.id.list);
+        lView.setAdapter(listAdapter);
     }
+
 }
