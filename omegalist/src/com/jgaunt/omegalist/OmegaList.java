45f57151ce680bcbbaf3903edaff9876c1170cc2
John (redfive) Gaunt <redfive@songbirdnest.com>
2012-08-28 12:28:44 -0700

    Access backing data and interact with object. - updated activity to be a ListActivity so we could - implement an onListItemClick method to access clicked list item - updated the id for the list view as listActivity requires it to be   @android:id/list

--- omegalist/src/com/jgaunt/omegalist/OmegaList.java
+++ omegalist/src/com/jgaunt/omegalist/OmegaList.java
@@ -1,18 +1,22 @@
 package com.jgaunt.omegalist;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class OmegaList extends Activity
+public class OmegaList extends ListActivity
 {
     public class OmegaListItem {
         public String omegaName = "";
+        public String omegaStatus = "";
 
-        public OmegaListItem( String aName ) {
+        public OmegaListItem( String aName, String aStatus ) {
             omegaName = aName;
+            omegaStatus = aStatus;
         }
 
         public String toString () {
@@ -29,14 +33,27 @@ public class OmegaList extends Activity
 
 
         OmegaListItem [] items = {
-                             new OmegaListItem("one"),
-                             new OmegaListItem("two"),
-                             new OmegaListItem("three") };
+                             new OmegaListItem("one", "Alpha"),
+                             new OmegaListItem("two", "Beta"),
+                             new OmegaListItem("three", "Gamma") };
 
         ArrayAdapter<OmegaListItem> listAdapter = new ArrayAdapter<OmegaListItem> (this, android.R.layout.simple_list_item_1, items);
 
-        ListView lView = (ListView) findViewById(R.id.list);
+        ListView lView = (ListView) findViewById(android.R.id.list);
         lView.setAdapter(listAdapter);
+    }
+
+    @Override
+    protected void onListItemClick( ListView aListView , View aView, int aPosition, long aId ) {
+        OmegaListItem item = (OmegaListItem) getListView().getItemAtPosition(aPosition);
+        showToast( "> " + item.toString() + " status: " + item.omegaStatus );
+        
+    }
+
+    /** Convenience method to pop a toast message on the screen */
+    private void showToast(String msg) {
+        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        error.show();
     }
 
 }
